d9dc3a515e1c537af0783a3e9e44a6f6e5faa6f3
Homeroso <johrodriguezgu@unal.edu.co>
2025-09-02 18:36:32 -0500

    Implement navbar styling

--- src/components/hero.js
+++ src/components/hero.js
@@ -15,6 +15,8 @@ const Hero = () => {
         const title = new SplitText('#hero h1', {type: 'chars, words'});
         const description = new SplitText('#hero-description', {type: 'words, lines'});
 
+        const last = ['#hero button', '#hero #square'];
+
         const textTL = gsap.timeline({ duration: 0.5, ease: 'power1.out' });
 
         textTL.from(prev.words, {
@@ -27,28 +29,35 @@ const Hero = () => {
             opacity: 0,
             y: 50,
             stagger: 0.2
+        }).from(last, {
+            opacity: 0,
+            y: 50,
         });
     });
 
     return (
         <section id='hero'>
-            <div className='flex flex-row w-60% min-h-dvh items-center content-center m-10 ml-20'>
+            <div className='flex flex-row w-60% min-h-dvh items-center m-10 ml-20'>
                 <div className='text-left p-8'>
                     <p>- Hello</p>
                     <div className='flex flex-row gap-2'>
-                    <h1 className='text-foreground'>I'm</h1>
-                    <h1>Homeroso</h1>
-                </div>
+                        <h1 className='text-foreground'>I'm</h1>
+                        <h1>Homeroso</h1>
+                    </div>
 
-                <p id='hero-description' className='text-2xl'>
-                    Designer & Developer crafting engaging digital experiences <br />with a focus on creativity and innovation.
-                </p>
-                <button >
-                    View Projects
-                </button>
+                    <p id='hero-description' className='text-2xl'>
+                        Designer & Developer crafting engaging digital experiences <br />with a focus on creativity and innovation.
+                    </p>
+                    <button>
+                        Contact
+                    </button>
+                </div>
+                <div className='flex'>
+                    <div className='absolute w-70 h-70 bg-secondary overflow-hidden ml-75 mt-[-175]' id='square'/>
+                    <div className='absolute w-70 h-70 border border-slate-50 overflow-hidden ml-50 mt-[-80]' id='square'/>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
     );
 };
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,10 +4,14 @@ const Navbar = () => {
     return (
         <nav>
             <div>
-                <ul><a href="#home">Home</a></ul>
-                <ul><a href="#about">About</a></ul>
-                <ul><a href="#projects">Projects</a></ul>
-                <ul><a href="#contact">Contact</a></ul>
+                <p>Homeroso</p>
+
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
             </div>
         </nav>
     );
